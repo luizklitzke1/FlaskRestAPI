5c7fec00c22a01a8b8e75be2498afd7aeef156a6
luizklitzke1 <luiz.klitzke1@gmail.com>
2022-08-06 15:42:46 -0300

    Setup inicial com mockup

---
+++ app/__init__.py
@@ -0,0 +1,25 @@
+import sys
+
+from flask import Flask, Blueprint
+from flask_restx import Api
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+from app.pessoa.pessoaController_v1 import api as home_ns
+
+app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
+blueprint = Blueprint('api', __name__)
+app.register_blueprint(blueprint)
+
+authorizations = {
+    'bearer': {
+        'name': "Authorization",
+        'in': "header",
+        'type': "apiKey",
+        'description': "Insert your JWT Token here!"
+    }
+}
+api = Api(app, title='Flask Experiments Api', version='1.0', description='Experiments with python flask',prefix='/api', authorizations=authorizations)
+
+
+api.add_namespace(home_ns, path='/pessoas')
